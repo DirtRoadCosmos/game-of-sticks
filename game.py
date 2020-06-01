c55ac202943fda3856b9ba818b77760ab3ba8dd1
andytilia <andytilia@gmail.com>
2020-06-01 00:38:41 +0000

    add first draft of ai logic

--- game.py
+++ game.py
@@ -13,17 +13,35 @@ player_type = ["", ""]
 
 print()
 player_type[0] == ""
-while not(player_type[0] == "ai" or player_type[0] == "human"):
-    player_type[0] = input("Will Player 1 be AI or Human? ").lower()
+while not(player_type[0] in ["r", "a", "h"]):
+    player_type[0] = input("Will Player 1 be (r)andom, (a)i or (h)uman? ").lower()
 
 player_type[1] == ""
-while not(player_type[1] == "ai" or player_type[1] == "human"):
-    player_type[1] = input("Will Player 2 be AI or Human? ").lower()
+while not(player_type[1] in ["r", "a", "h"]):
+    player_type[1] = input("Will Player 2 be (r)andom, (a)i or (h)uman? ").lower()
 
+starting_sticks = 21
 playing = True
 wins = [0, 0]
+hats = []
+hats.append([])
+hats.append([])
+hats[1] = []
+waiting = []
+waiting.append([])
+waiting.append([])
+
+def full_hat():
+    return [1, 2, 3]
+
+for i in range(starting_sticks):
+    hats[0].append(full_hat())
+    hats[1].append(full_hat())
+    waiting[0].append(None)
+    waiting[1].append(None)
+
 while playing:
-    sticks = 21
+    sticks = starting_sticks
     player = 1
     while sticks > 0:
         # print start of turn
@@ -31,15 +49,20 @@ while playing:
         print(f"Player {player}: It's your turn.")
         print(f"There are {sticks} sticks on the table.")
 
-        if player_type[player-1] == "human":
+        if player_type[player-1] == "r":
+            pickup = randint(1, 3)
+            print(f"Random picks up {pickup} sticks.")
+        elif player_type[player-1] == "a":
+            options = hats[player-1][sticks-1]
+            pickup = options.pop(randint(0, len(options)-1))
+            waiting[player-1][sticks-1] = pickup
+            print(f"AI picks up {pickup} sticks.")
+        elif player_type[player-1] == "h":
             # get player input
             pickup = input("How many sticks do you take? ")
             while not pickup.isdigit() or int(pickup) < 1 or int(pickup) > 3:
                 pickup = input("How many sticks do you take? ")
             pickup = int(pickup)
-        else:
-            pickup = randint(1, 3)
-            print(f"AI picks up {pickup} sticks.")
 
         # update state variables
         sticks = sticks - pickup
@@ -54,12 +77,32 @@ while playing:
     wins[player-1] += 1
     print(f"That makes it {wins[0]}-{wins[1]}")
 
+    # update winning AI
+    if player_type[player-1] == "a":
+        for idx, item in enumerate(waiting[player-1]):
+            if item:
+                hats[player-1][idx].extend([item, item])
+
+    # for both players
+    for i in range(2):
+        if player_type[i] == "a":
+            # clear waiting
+            for j in waiting[i]:
+                j = None
+            # fill empty hats
+            for hat in hats[i]:
+                if len(hat) == 0:
+                    hat.extend(full_hat())
+            # print hats
+            print()
+            print(f"Updated hats for Player {i+1}:")
+            print(hats[i])
+
     print()
     again = input("Press ENTER to play again, q-ENTER to exit. ")
     print()
     if again == "q":
         playing = False
-
 
 
 
